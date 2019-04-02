902a835fef8c0c61965cd19a6023cd0cc40c9aa4
malekshero <malekshero@gmail.com>
2019-04-03 02:32:38 +0300

    "CSV Reader"

---
+++ src/ReadCSVData.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadCSVData {
+
+    public static void main(String[] args) {
+
+        String csvFile = "assest/read.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            Scanner scan = new Scanner(System.in);
+// select * --> line
+// out put coloumn country[ColNumber]
+            int ColNumber = scan.nextInt()-1;
+            while ((line = br.readLine()) != null) {
+                String[] country = line.split(cvsSplitBy);
+                System.out.println( line);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
